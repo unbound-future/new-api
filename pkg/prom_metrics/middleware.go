97f1949ae50decf7c57ef1de91475b0713c4f39f
rxopy <zflmiy@gmail.com>
2026-05-20 13:26:04 +0800

    chore(prom_metrics): remove debug log for channel labels

--- pkg/prom_metrics/middleware.go
+++ pkg/prom_metrics/middleware.go
@@ -1,7 +1,6 @@
 package prom_metrics
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 	"time"
@@ -58,8 +57,6 @@ func (m *metrics) GinMiddleware() gin.HandlerFunc {
 		channelLabel := strconv.Itoa(channelId)
 		channelName := common.GetContextKeyString(c, constant.ContextKeyChannelName)
 		channelType := common.GetContextKeyInt(c, constant.ContextKeyChannelType)
-		// DEBUG: 临时日志，定位 channel_name 为空的问题
-		common.SysLog(fmt.Sprintf("[prom_metrics-debug] channelId=%d, channelName=%q, channelType=%d, path=%s", channelId, channelName, channelType, path))
 		cNameLabel, cTypeLabel := m.channelLabels(channelId, channelName, channelType)
 		isStreamLabel := strconv.FormatBool(common.GetContextKeyBool(c, constant.ContextKeyIsStream))
 
